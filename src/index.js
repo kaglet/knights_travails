25a23fecb52668bc429701d0b8cedc62e6175ce8
Kago <kmotlhabane769@gmail.com>
2024-07-16 16:58:40 +0200

    Reduce size of board and goal path for easy debugging

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import Graph from "./graph/graph.js";
 import { knightMoves } from "./knights_travails.js";
 
-let graph = new Graph(8);
+let graph = new Graph(4);
 
 console.log(graph.grid);
 
-knightMoves([3,3], [4,3], graph);
+knightMoves([0,0], [3,3], graph); // answer is [0,0] -> [1,2] -> [3,3]
