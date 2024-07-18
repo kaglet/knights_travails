288ee6556c165c07ee3a81e2ab142ffd6285f7dd
Kago <kmotlhabane769@gmail.com>
2024-07-18 16:47:41 +0200

    Add prompts and output new build

--- dist/app.bundle.js
+++ dist/app.bundle.js
@@ -476,7 +476,7 @@ class Graph {
     }
 }
 
-/* harmony default export */ const graph = (Graph);
+/* harmony default export */ const graph_graph = (Graph);
 // EXTERNAL MODULE: ./node_modules/@datastructures-js/queue/index.js
 var _datastructures_js_queue = __webpack_require__(595);
 ;// CONCATENATED MODULE: ./src/knights_travails.js
@@ -570,10 +570,48 @@ function knightMoves(start, end, graph) {
 
 
 
-// TODO: Add input mechanism to specify graph size and enter inputs
-let src_graph = new graph(8);
+let inputValid = true;
 
-knightMoves([3,3], [4,3], src_graph); 
+let size = +prompt("Enter graph size", 8); // [3,3]
+
+if (typeof size !== "number") {
+    alert("Enter number size. Refresh page to start over.");
+    inputValid = false;
+} 
+
+
+let start = JSON.parse(prompt("Enter start coordinates in array form", "[0,0]")); // [3,3]
+
+if (!Array.isArray(start)) {
+    alert("Enter start in correct array form. Refresh page to start over.");
+    inputValid = false;
+} else if (typeof start[0] !== "number" || typeof start[1] !== "number") {
+    alert("Enter number values for coordinates. Refresh page to start over.");
+    inputValid = false;
+} 
+
+let end = JSON.parse(prompt("Enter end coordinates in array form", "[3,3]")); // [4,3]
+
+if (!Array.isArray(end)) {
+    alert("Enter start in correct array form. Refresh page to start over.");
+    inputValid = false;
+} else if (typeof end[0] !== "number" || typeof end[1] !== "number") {
+    alert("Enter number values for coordinates. Refresh page to start over.");
+    inputValid = false;
+} 
+
+if (inputValid) {
+    let graph = new graph_graph(size);
+    knightMoves(start, end, graph); 
+} else {
+    alert("Refresh page due to input errors.")
+}
+
+
+
+
+
+
 })();
 
 /******/ })()
--- src/index.js
+++ src/index.js
@@ -1,7 +1,44 @@
 import Graph from "./graph/graph.js";
 import { knightMoves } from "./knights_travails.js";
 
-// TODO: Add input mechanism to specify graph size and enter inputs
-let graph = new Graph(8);
+let inputValid = true;
 
-knightMoves([3,3], [4,3], graph); 
+let size = +prompt("Enter graph size", 8); // [3,3]
+
+if (typeof size !== "number") {
+    alert("Enter number size. Refresh page to start over.");
+    inputValid = false;
+} 
+
+
+let start = JSON.parse(prompt("Enter start coordinates in array form", "[0,0]")); // [3,3]
+
+if (!Array.isArray(start)) {
+    alert("Enter start in correct array form. Refresh page to start over.");
+    inputValid = false;
+} else if (typeof start[0] !== "number" || typeof start[1] !== "number") {
+    alert("Enter number values for coordinates. Refresh page to start over.");
+    inputValid = false;
+} 
+
+let end = JSON.parse(prompt("Enter end coordinates in array form", "[3,3]")); // [4,3]
+
+if (!Array.isArray(end)) {
+    alert("Enter start in correct array form. Refresh page to start over.");
+    inputValid = false;
+} else if (typeof end[0] !== "number" || typeof end[1] !== "number") {
+    alert("Enter number values for coordinates. Refresh page to start over.");
+    inputValid = false;
+} 
+
+if (inputValid) {
+    let graph = new Graph(size);
+    knightMoves(start, end, graph); 
+} else {
+    alert("Refresh page due to input errors.")
+}
+
+
+
+
+
